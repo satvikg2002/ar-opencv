44f2bc2093afe3dee43f5814b6e932a4aafadeb7
Satvik <satvik.noida@gmail.com>
2024-05-04 12:27:32 +0530

    hough circle detection

--- python-backend/circle_detect.py
+++ python-backend/circle_detect.py
@@ -11,6 +11,12 @@ print(capture.get(cv2.CAP_PROP_FPS))
 t = 100     # threshold of canny edge detector
 w = 640     # width of image for uniformity
 
+# hough circle params
+sc = 1      # scale
+md = 30     # minimum distance between 2 circles
+at = 40     # accumulator threshold, small numbers are more sensitive to false pos but make detection tolerant
+    
+
 while True:
     ret, image = capture.read()
 
@@ -28,11 +34,26 @@ while True:
     grid[0:h, 0:w] = image
 
     # rgb to greyscale 8bit
-    grid[h:2*h, 0:w] = np.dstack([cv2.Canny(grey, t / 2, t)] * 3)
-    grid[0:h, w:2*w] = np.dstack([blurred] * 3)
-    grid[h:2*h, w:2*w] = np.dstack([cv2.Canny(blurred, t / 2, t)] * 3)
+    grid[h:2*h, 0:w] = np.dstack([cv2.Canny(grey, t / 2, t)] * 3)       # edge detection on greyscale
+    grid[0:h, w:2*w] = np.dstack([blurred] * 3)                         # blurred grey image
+    grid[h:2*h, w:2*w] = np.dstack([cv2.Canny(blurred, t / 2, t)] * 3)  # edge detection on blurred greyscale (reduces bg noise)
 
-    cv2.imshow("Image previews", grid)
+    # cv2.imshow("Image previews", grid)
+
+    # hough gradients for circle detection
+    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, sc, md, t, at)
+
+    if circles is not None and circles[0][0].ndim == 1:
+        circle = circles[0][0]  # first circle
+        x, y, radius = int(circle[0]), int(circle[1]), int(circle[2])
+        print(x, y, radius)
+
+        # Highlight the circle
+        cv2.circle(image, (x, y), radius, (0, 0, 255), 2)
+        # Draw dot in the center
+        cv2.circle(image, (x, y), 1, (0, 0, 255), 3) 
+
+    cv2.imshow('Image with detected circle', image)
 
     # press q to break
     if cv2.waitKey(1) & 0xFF == ord('q'):
